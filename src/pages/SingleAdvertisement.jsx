65191a0bbc2ae424201021d4043a6921f477a3e1
Nurbek Avtandilov <avtandilov06@gmail.com>
2025-03-02 21:03:53 +0600

    add some changes

--- src/pages/SingleAdvertisement.jsx
+++ src/pages/SingleAdvertisement.jsx
@@ -21,6 +21,7 @@ const SingleAdvertisement = ({ item, lang, onBackHandler }) => {
     const [searchParams] = useSearchParams();
     const [name, setName] = useState('');
     const [houses, setHouses] = useState([]);
+    const byLink = searchParams.get('bylink');
 
     const {
         ref,
@@ -153,15 +154,16 @@ const SingleAdvertisement = ({ item, lang, onBackHandler }) => {
             <div className={clsx('single-result-card', { 'card-padding': selected.length && houses.length })}>
                 <div className="">
                     <div className="single-card">
-                        {item.photo_ids && (
+                        {item.photo_ids && !byLink && (
                             <ImageSlider imageIds={item.photo_ids} />
                         )}
                         <div className="card-detail single-card-detail">
-                            {item.name && (<p><span>{DICTIONARY[lang].nameLabel}:</span> {item.name}</p>)}
-                            <p><span>{DICTIONARY[lang].city}:</span> {item.city}</p>
-                            <p><span>{DICTIONARY[lang].address}:</span> <a href={`https://2gis.kg/search/${encodeURIComponent(item.city + ' ' + item.address)}`} target='_blank'>{item.address}</a></p>
-                            <p><span>{DICTIONARY[lang].roomCount}:</span> {item.room_count}</p>
-                            <div className="card-prices single-card-prices">
+                            {item.name && (<p className={clsx({ 'bold-title': byLink })}>{!byLink && <span>{DICTIONARY[lang].nameLabel}:</span>} {item.name}</p>)}
+                            {!byLink && <p><span>{DICTIONARY[lang].city}:</span> {item.city}</p>}
+                            {!byLink && <p><span>{DICTIONARY[lang].address}:</span> <a href={`https://2gis.kg/search/${encodeURIComponent(item.city + ' ' + item.address)}`} target='_blank'>{item.address}</a></p>}
+                            {/* <p><span>{DICTIONARY[lang].roomCount}:</span> {item.count}</p> */}
+                            {!byLink && (
+                                <div className="card-prices single-card-prices">
                                 {Object.entries(item.price).map(([key, value]) => {
                                     if (!value) {
                                         return null;
@@ -172,37 +174,31 @@ const SingleAdvertisement = ({ item, lang, onBackHandler }) => {
                                     );
                                 })}
                             </div>
+                            )}
 
                             {/* <div className='card-status'>
                                 {item.active ? <div className='free'>{DICTIONARY[lang].free}</div> : <div className='busy'>{DICTIONARY[lang].busy}</div>}
                             </div> */}
 
-                            {show ? (
-                                <div>
-
-                                    <div className='phone-number' onClick={copyHandler}>
-                                        {item.phone}
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-copy"><rect width="14" height="14" x="8" y="8" rx="2" ry="2" /><path d="M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2" /></svg>
+                            {!byLink && (
+                                show ? (
+                                    <div>
+    
+                                        <div className='phone-number' onClick={copyHandler}>
+                                            {item.phone}
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-copy"><rect width="14" height="14" x="8" y="8" rx="2" ry="2" /><path d="M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2" /></svg>
+                                        </div>
+    
+                                        {showText && (<p className='copy-text'>{DICTIONARY[lang].numberCopied}</p>)}
                                     </div>
-
-                                    {showText && (<p className='copy-text'>{DICTIONARY[lang].numberCopied}</p>)}
-                                </div>
-                            ) : (
-                                <div className='call-btn' onClick={(e) => callHandler(e, item._id, item.owner_id, item.phone)}>
-                                    {DICTIONARY[lang].showNumber}
-                                </div>
+                                ) : (
+                                    <div className='call-btn' onClick={(e) => callHandler(e, item._id, item.owner_id, item.phone)}>
+                                        {DICTIONARY[lang].showNumber}
+                                    </div>
+                                )
                             )}
                         </div>
                     </div>
-                </div>
-
-                <div className='houses-container'>
-                <p>Выберите номер дома для бронирования:</p>
-                <div className='houses-list'>
-                    {housesList.map((obj) => (
-                        <HouseItem number={obj.number} disabled={obj.disabled} setHouses={setHouses} />
-                    ))}
-                </div>
                 </div>
 
                 <div className='book-calendar'>
@@ -220,6 +216,15 @@ const SingleAdvertisement = ({ item, lang, onBackHandler }) => {
                             booked: "my-booked-class"
                         }}
                     />
+                </div>
+
+                <div className='houses-container'>
+                <p>Выберите номер дома для бронирования:</p>
+                <div className='houses-list'>
+                    {housesList.map((obj) => (
+                        <HouseItem number={obj.number} disabled={obj.disabled} setHouses={setHouses} />
+                    ))}
+                </div>
                 </div>
 
                 {/* <div className="field-wrapper">
